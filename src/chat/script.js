fdc415f7265cd55c6ad2bfde508570f2c86499be
HezekiahIvandi <hezekiahivandi@gmail.com>
2024-03-03 00:06:42 +0700

    Progress (3), menambahkan script DOM manip untuk fitur chat

--- src/chat/script.js
+++ src/chat/script.js
@@ -1,24 +1,52 @@
-const taskifySelectorBtn = document.querySelector('#taskify')
-const hezeSelectorBtn = document.querySelector('#hezekiah')
-const vieSelectorBtn = document.querySelector('#vie')
-const marchellaSelectorBtn = document.querySelector('#marchella')
-
-
-const chatHeader = document.querySelector('.chat-box-header')
+const contactLists = document.querySelectorAll(".contact");
+const names = document.querySelectorAll(".name");
+const chatHeader = document.querySelector('.chat-header-title')
+const chatCurrentName = document.querySelector('.chat-current-contact-name')
 const chatMessages = document.querySelector('.chat-bubble-container')
 const chatInputForm = document.querySelector('.chat-input-form')
-const chatInput = document.querySelector('.text-area')
+const chatTextArea = document.querySelector('.text-area')
 const clearChatBtn = document.querySelector('.clear-button')
+
+const updateCurrentContact = (nameHeader)=>{
+    chatHeader.innerText = nameHeader;
+    chatCurrentName.innerText = nameHeader;
+}
+
+contactLists.forEach((contact)=>{
+    //current contact's name
+    const name = contact.querySelector('.name').textContent;
+
+    //click event for contact lists
+    contact.addEventListener('click', ()=>{
+        contactLists.forEach((con)=>{
+            con.classList.remove('contact-selected');
+        });
+       contact.classList.add('contact-selected');
+
+       updateCurrentContact(name);
+    });
+});
+
 
 const createChatBubble = (text) =>`
 <div class="my-chat-bubble">
     <div class="my-chat-bubble-text">
         <p>You</p>
         <div class="my-bubble">
-            <p class="latest-chat">Hi Hezekiah, Good morning!</p>
+            <p class="latest-chat">${text}</p>
         </div>
     </div>
     <div><img src="../../assets/Pfp.png" alt=""></div>
 </div>
 `
+const displayChatBubble = (event) =>{
+    event.preventDefault();
+    const message = {
+        text: chatTextArea.value
+    }
 
+    chatMessages.innerHTML += createChatBubble(message.text);
+
+}
+
+chatInputForm.addEventListener('submit', displayChatBubble);
