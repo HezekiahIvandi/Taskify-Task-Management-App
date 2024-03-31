ba723e491c647e17bea2804c2e43cd9be01d4ac9
Atlan22x <atlan22x@gmail.com>
2024-03-31 21:38:59 +0700

    Menambahkan Fungsionalitas Registrasi Login dan Autentikasi

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const port = 3000;
 const bodyParser = require('body-parser');
+const flash = require("connect-flash");
+const session = require("express-session");
+const passport = require("passport");
 
 const chatRoutes = require("./routes/chatRoutes");
 const homeRoutes = require("./routes/homeRoutes");
@@ -13,6 +16,9 @@ const authRoutes = require("./routes/authRoutes");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 require('dotenv').config();
+
+//passport config
+require("./config/passport")(passport);
 
 // Inisialisasi koneksi ke MongoDB menggunakan mongoose
 mongoose.connect(process.env.MONGODB_URI)
@@ -26,9 +32,34 @@ mongoose.connect(process.env.MONGODB_URI)
     app.use(expressLayouts);
     app.set('layout extractScripts', true);
 
+    // express session middleware
+    app.use(express.urlencoded({ extended: false }));
+    app.use(
+      session({
+        secret: "secret",
+        resave: true,
+        saveUninitialized: true,
+      })
+    );
+
+    // Passport middleware
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    //connect flash
+    app.use(flash());
+
     // Menyimpan current url ke variable lokal currentPage (variable digunakan di navbar.ejs)
     app.use((req, res, next) => {
       res.locals.currentPage = req.path;
+      next();
+    });
+
+    // global var
+    app.use((req, res, next) => {
+      res.locals.success_msg = req.flash("success_msg");
+      res.locals.error_msg = req.flash("error_msg");
+      res.locals.error = req.flash("error");
       next();
     });
 
@@ -38,13 +69,15 @@ mongoose.connect(process.env.MONGODB_URI)
     app.use(chatRoutes);
     app.use(authRoutes);
 
+
+
     // Membuat koneksi ke MongoDB saat aplikasi dimulai
 
     async function startApp() {
       try {
         const client = await mongoose.connection.getClient(); // Menggunakan getClient() untuk mendapatkan instance MongoClient
         console.log('Connected to MongoDB');
-       
+
       } catch (error) {
         console.error('Error starting app', error);
       }
@@ -54,7 +87,7 @@ mongoose.connect(process.env.MONGODB_URI)
     app.listen(port, () => {
       console.log(`Server is up on port http://localhost:${port}`);
     });
-    
+
     // Panggil fungsi startApp untuk memulai aplikasi
     startApp();
   })
@@ -62,4 +95,3 @@ mongoose.connect(process.env.MONGODB_URI)
     console.error('MongoDB connection error:', err.message);
   });
 
-  
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
+const User = require("../models/User");
+
 router.get("/login", (req, res) => {
   res.render("login.ejs", {
     title: "Login",
@@ -16,5 +20,98 @@ router.get("/register", (req, res) => {
     js: "js/register.js",
     layout: "mainLayout.ejs",
   });
+});
+
+router.post("/register", (req, res) => {
+  const { name, email, password, password2 } = req.body;
+  // console.log(req.body);
+  // res.send("hello");
+  let errors = [];
+
+  //cek required
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: "harap data di input semua" });
+  }
+
+  //password
+  if (password !== password2) {
+    errors.push({ msg: "password tidak sama" });
+  }
+
+  if (errors.length > 0) {
+    res.render("register", {
+      errors,
+      name,
+      email,
+      password,
+      password2,
+      title: "Register",
+      css: "css/register.css",
+      js: "js/register.js",
+      layout: "mainLayout.ejs",
+    });
+  } else {
+    //validasi oke lanjut database
+    User.findOne({ email: email }).then((user) => {
+      if (user) {
+        //usernya ada
+        errors.push({ msg: "Email sudah terdaftar" });
+        res.render("register", {
+          errors,
+          name,
+          email,
+          password,
+          password2,
+          title: "Register",
+          css: "css/register.css",
+          js: "js/register.js",
+          layout: "mainLayout.ejs",
+        });
+      } else {
+        const newUser = new User({
+          name,
+          email,
+          password,
+        });
+        //hash password
+        bcrypt.genSalt(10, (err, salt) =>
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            //set password jadi hash
+            newUser.password = hash;
+
+            //simpan user
+            newUser
+              .save()
+              .then((user) => {
+                req.flash(
+                  "success_msg",
+                  "Anda berhasil registrasi, Silahkan Login"
+                );
+
+                res.redirect("/login");
+              })
+              .catch((err) => console.log(err));
+          })
+        );
+      }
+    });
+  }
+});
+
+//login handle
+router.post("/login", async (req, res, next) => {
+  passport.authenticate("local", {
+    successRedirect: "/project",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })(req, res, next);
+});
+
+//logout handle
+router.get("/logout", (req, res) => {
+  req.logout();
+  req.flash("success_msg", "Anda berhasil Log out");
+  res.redirect("/login");
 });
 module.exports = router;
