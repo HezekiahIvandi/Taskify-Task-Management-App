a66ee5782538f3e29197ae2bc8238ba82486241f
HezekiahIvandi <hezekiahivandi@gmail.com>
2024-04-01 11:25:49 +0700

    Fungsionalitas CRUD pada halaman chat

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const port = 3000;
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const flash = require("connect-flash");
 const session = require("express-session");
 const passport = require("passport");
@@ -15,61 +15,61 @@ const authRoutes = require("./routes/authRoutes");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-require('dotenv').config();
+require("dotenv").config();
 
 //passport config
 require("./config/passport")(passport);
 
 // Inisialisasi koneksi ke MongoDB menggunakan mongoose
-mongoose.connect(process.env.MONGODB_URI)
-  .then(async () => {
-    console.log('MongoDB connected');
+mongoose.connect(process.env.MONGODB_URI).then(async () => {
+  console.log("MongoDB connected");
 
-    // Gunakan body-parser middleware
-    app.use(bodyParser.json());
-    app.set("view engine", "ejs");
-    app.use(express.static("public"));
-    app.use(expressLayouts);
-    app.set('layout extractScripts', true);
+  // Gunakan body-parser middleware
+  app.use(bodyParser.json());
+  app.set("view engine", "ejs");
+  app.use(express.static("public"));
+  app.use(expressLayouts);
+  app.set("layout extractScripts", true);
 
-    // express session middleware
-    app.use(express.urlencoded({ extended: false }));
-    app.use(
-      session({
-        secret: "secret",
-        resave: true,
-        saveUninitialized: true,
-      })
-    );
+  // express session middleware
+  app.use(express.urlencoded({ extended: false }));
+  app.use(
+    session({
+      secret: "secret",
+      resave: true,
+      saveUninitialized: true,
+    })
+  );
 
-    // Passport middleware
-    app.use(passport.initialize());
-    app.use(passport.session());
+  // Passport middleware
+  app.use(passport.initialize());
+  app.use(passport.session());
 
-    //connect flash
-    app.use(flash());
+  //connect flash
+  app.use(flash());
 
-    // Menyimpan current url ke variable lokal currentPage (variable digunakan di navbar.ejs)
-    app.use((req, res, next) => {
-      res.locals.currentPage = req.path;
-      next();
-    });
+  // Menyimpan current url ke variable lokal currentPage (variable digunakan di navbar.ejs)
+  app.use((req, res, next) => {
+    res.locals.currentPage = req.path;
+    next();
+  });
 
-    // global var
-    app.use((req, res, next) => {
-      res.locals.success_msg = req.flash("success_msg");
-      res.locals.error_msg = req.flash("error_msg");
-      res.locals.error = req.flash("error");
-      next();
-    });
+  // global var
+  app.use((req, res, next) => {
+    res.locals.success_msg = req.flash("success_msg");
+    res.locals.error_msg = req.flash("error_msg");
+    res.locals.error = req.flash("error");
+    next();
+  });
 
-    // Route handling
-    app.use(homeRoutes);
-    app.use(projectRoutes);
-    app.use(chatRoutes);
-    app.use(authRoutes);
+  // Route handling
+  app.use(homeRoutes);
+  app.use(projectRoutes);
+  app.use(chatRoutes);
+  app.use(authRoutes);
 
-//Memulai server
-app.listen(port, () => {
-  console.log(`server is up on port localhost:${port}`);
+  //Memulai server
+  app.listen(port, () => {
+    console.log(`server is up on port http://localhost:${port}`);
+  });
 });
