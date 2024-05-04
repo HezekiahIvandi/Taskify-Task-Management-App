eefdc53c27a218f2f6ebb8105c7b02b15f44f7a2
HezekiahIvandi <hezekiahivandi@gmail.com>
2024-05-04 15:38:17 +0700

    Menambah kan  read dan delete users pada halaman dashboard

--- controller/adminController.js
+++ controller/adminController.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const ContactSchema = require("../models/contactModel"); // Import Contact model
+const UserSchema = require("../models/User");
+const ContactListSchema = require("../models/contactListModel");
+const {
+  updateSavedContactsId,
+  deleteSavedContactId,
+} = require("../controller/contactListController");
 
 const renderDashboard = async (req, res) => {
+  const users = await UserSchema.find();
   const currentUser = req.user.name;
   const currentUserPfp = req.user.photoUrl;
   res.render("dashboard.ejs", {
@@ -10,7 +18,40 @@ const renderDashboard = async (req, res) => {
     layout: "mainLayout.ejs",
     username: currentUser,
     photoUrl: currentUserPfp,
+    users: users,
   });
 };
 
-module.exports = { renderDashboard };
+const deleteUserById = async (req, res) => {
+  const id = req.params.id;
+  console.log("id is ", id);
+  try {
+    // Find the user by ID and remove it from the database
+    await UserSchema.deleteOne({ _id: id })
+      .then(() => {
+        res.status(200).json({
+          msg: "Contact successfully deleted",
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({ msg: "Unable to delete the contact" });
+      });
+  } catch (err) {
+    // Handle any errors that occurred during the delete operation
+    console.error("Error deleting user:", err);
+    throw err;
+  }
+};
+
+const getAllUsers = async (req, res) => {
+  try {
+    const usersData = await UserSchema.find();
+    res.status(200).json({ users: usersData });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: "Unable to get users" });
+  }
+};
+
+module.exports = { renderDashboard, deleteUserById, getAllUsers };
